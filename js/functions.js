9319da7eb6404fcb0eae93c8d0f541a0fa1f5518
FerrariMarc0 <marco1010@live.it>
2023-03-02 19:30:15 +0100

    bonus

--- js/functions.js
+++ js/functions.js
@@ -4,10 +4,10 @@ function newElements(a, b, c, d){
     const element= document.createElement(a);
     element.classList.add(b);
     element.innerHTML= c;
+    
     element.addEventListener('click', function(){
         console.log(c);
         element.classList.add(d);
     })
-
     return element;
 }
--- js/main.js
+++ js/main.js
@@ -7,10 +7,32 @@ const gameLevel= document.getElementById('difficulty');
 
 button.addEventListener('click',
  function(){
-    button.disabled=true;
-    for(let i = 1; i <= 100; i++){
-    const createdElement= newElements('div', 'box', i, 'azure');
-    bigContainer.append(createdElement);
-}
-    
-})
+    const inputLevel= difficulty.value;
+    switch (inputLevel){
+
+        case 'easy':
+            bigContainer.innerHTML= '';
+            for(let i = 1; i <= 100; i++){
+                const createdElement= newElements('div', 'box', i, 'azure');
+                bigContainer.append(createdElement);
+            }
+        break;
+        
+        case 'normal':
+            bigContainer.innerHTML= '';
+            for(let i = 1; i <= 81; i++){
+                const createdElement= newElements('div', 'box_81', i, 'azure');
+                bigContainer.append(createdElement);
+            }
+        break;
+
+        case 'hard':
+            bigContainer.innerHTML= '';
+            for(let i = 1; i <= 49; i++){
+                const createdElement= newElements('div', 'box_49', i, 'azure');
+                bigContainer.append(createdElement);
+        
+            }
+    }
+
+});
